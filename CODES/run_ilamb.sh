339fc4f0833263d8eef459b7fd33a911a0072228
Mark Piper <mark.piper@colorado.edu>
2016-03-15 13:43:19 -0600

    Tar the output from the ILAMB run

--- CODES/run_ilamb.sh
+++ CODES/run_ilamb.sh
@@ -6,14 +6,20 @@
 # ILAMB execution script adapted to run on `beach` through its queue
 # manager. Submit this script with:
 #
-#   $ qsub run_ilamb.sh
-# 
+#   $ qsub /scratch/pbs/ilamb/CODES/run_ilamb.sh
+#
+# The output from the ILAMB run is saved to a tarball in the
+# ILAMB_ROOT directory, `/scratch/pbs/ilamb`.
+#
+# Note that ILAMB will clobber the results of the previous run, which
+# are stored in the static $ILAMB_ROOT/OUTPUT directory.
 
 # Configure ILAMB dependencies and paths.
+export ILAMB_ROOT=/scratch/pbs/ilamb
 tools_dir=/home/csdms/tools
 export NCARG_ROOT=$tools_dir/ncl
 PATH=$NCARG_ROOT/bin:$tools_dir/ImageMagick/bin:$PATH
-export ILAMB_ROOT=/scratch/pbs/ilamb
+job_id=`basename $PBS_JOBID .beach.colorado.edu`
 
 # Define model simulation type, CLM or CMIP5.
 export MODELTYPE=CMIP5
@@ -24,8 +30,10 @@ export SPATRES=0.5x0.5
 # Define plot file type, i.e., eps, gif, pdf, png, ps.
 export PLOTTYPE=png
 
-cd $PBS_O_WORKDIR
-echo $ILAMB_ROOT
+cd $ILAMB_ROOT ; echo $ILAMB_ROOT
 date
+cd CODES
 ncl -n main_ncl_code.ncl
+cd $ILAMB_ROOT
+tar zcf ILAMB.OUTPUT.$job_id.tar.gz -C $ILAMB_ROOT OUTPUT
 date
