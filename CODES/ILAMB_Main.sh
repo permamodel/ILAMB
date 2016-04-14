abb2560f931df0d9f79e6881adbc011412562328
Mark Piper <mark.piper@colorado.edu>
2016-04-14 11:31:14 -0600

    Implement environment variables for ILAMB subdirectories

--- CODES/ILAMB_Main.sh
+++ CODES/ILAMB_Main.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
+#
 # ILAMB execution script.
+#
+# Run with:
+#   $ ./ILAMB_Main.sh > ILAMB.stdout
 
-export ILAMB_UNDER=`pwd`
+export ILAMB_CODESDIR=`pwd`
 cd ..
 export ILAMB_ROOT=`pwd`
+cd $ILAMB_CODESDIR
 
-cd $ILAMB_UNDER
+# Allow a user to configure these directories.
+export ILAMB_DATADIR=$ILAMB_ROOT/DATA
+export ILAMB_MODELSDIR=$ILAMB_ROOT/MODELS
+export ILAMB_OUTPUTDIR=$ILAMB_ROOT/OUTPUT
 
-echo $ILAMB_ROOT
+echo "ILAMB directories:"
+echo "ILAMB_ROOT      $ILAMB_ROOT"
+echo "ILAMB_CODESDIR  $ILAMB_CODESDIR"
+echo "ILAMB_DATADIR   $ILAMB_DATADIR"
+echo "ILAMB_MODELSDIR $ILAMB_MODELSDIR"
+echo "ILAMB_OUTPUTDIR $ILAMB_OUTPUTDIR"
 
 ## Define model simulation type, CLM or CMIP5.
 export MODELTYPE=CMIP5
@@ -16,8 +29,8 @@ export MODELTYPE=CMIP5
 export SPATRES=0.5x0.5
 
 ## Define plot file type, i.e., eps, gif, pdf, png, ps.
-export PLOTTYPE=pdf
+export PLOTTYPE=png
 
 date
-ncl -n ./main_ncl_code.ncl
+ncl -n main_ncl_code.ncl
 date
