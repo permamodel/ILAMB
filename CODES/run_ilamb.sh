d424535164bb1887c09a978c33accd0e14ce75a2
Mark Piper <mark.piper@colorado.edu>
2016-04-28 10:20:12 -0600

    Add env vars for ILAMB subdirectories

--- CODES/run_ilamb.sh
+++ CODES/run_ilamb.sh
@@ -8,8 +8,8 @@
 #
 #   $ qsub /scratch/pbs/ilamb/CODES/run_ilamb.sh -m ae -M <email>
 #
-# The output from the ILAMB run is saved to a tarball in the
-# ILAMB_ROOT directory, `/scratch/pbs/ilamb`.
+# The output from the ILAMB run is saved to a tarball in the user's
+# working directory, along with PBS output and error logs.
 #
 # Note that ILAMB will clobber the results of the previous run, which
 # are stored in the static $ILAMB_ROOT/OUTPUT directory.
@@ -23,23 +23,28 @@ export SPATRES=0.5x0.5
 # Define plot file type: eps, pdf, png, or ps.
 export PLOTTYPE=png
 
-# Configure ILAMB paths and dependencies.
-export ILAMB_ROOT=/scratch/pbs/ilamb
+# Configure ILAMB deps NCL and ImageMagick.
 tools_dir=/home/csdms/tools
 export NCARG_ROOT=$tools_dir/ncl
 PATH=$NCARG_ROOT/bin:$tools_dir/ImageMagick/bin:$PATH
-job_id=`basename $PBS_JOBID .beach.colorado.edu`
-stdout_file=$ILAMB_ROOT/OUTPUT/ILAMB.stdout
-stderr_file=$ILAMB_ROOT/OUTPUT/ILAMB.stderr
+
+# Configure ILAMB paths.
+export ILAMB_ROOT=/scratch/pbs/ilamb
+export ILAMB_CODESDIR=$ILAMB_ROOT/CODES
+export ILAMB_DATADIR=$ILAMB_ROOT/DATA
+export ILAMB_MODELSDIR=$ILAMB_ROOT/MODELS
+export ILAMB_OUTPUTDIR=$ILAMB_ROOT/OUTPUT
+stdout_file=$ILAMB_OUTPUTDIR/ILAMB.stdout
+stderr_file=$ILAMB_OUTPUTDIR/ILAMB.stderr
 
 # Run ILAMB.
-cd $ILAMB_ROOT ; echo $ILAMB_ROOT
-cd CODES
+cd $ILAMB_CODESDIR
 echo "ILAMB start:" `date`
 ncl -n main_ncl_code.ncl 1> $stdout_file 2> $stderr_file
 echo "ILAMB finish:" `date`
 
 # Package results.
+job_id=`basename $PBS_JOBID .beach.colorado.edu`
 tarfile=ILAMB-$PBS_O_LOGNAME-$job_id.tar.gz
 tar zcf $tarfile -C $ILAMB_ROOT OUTPUT
 mv $tarfile $PBS_O_WORKDIR
