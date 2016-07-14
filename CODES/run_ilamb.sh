cbb5a9ee2c741a8c5f42916aa1dcda6649bcf1e3
Mark Piper <mark.piper@colorado.edu>
2016-07-14 15:30:35 -0600

    Configure script to use /nas/data

--- CODES/run_ilamb.sh
+++ CODES/run_ilamb.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=4:00:00
+#PBS -l walltime=2:00:00
 #PBS -N ILAMB
 #
 # ILAMB execution script adapted to run on `beach` through its queue
 # manager. Submit this script with:
 #
-#   $ qsub /scratch/pbs/ilamb/CODES/run_ilamb.sh -m ae -M <email>
+#   $ qsub /home/csdms/tools/ILAMB/CODES/run_ilamb.sh -m ae -M <email>
 #
 # The output from the ILAMB run is saved to a tarball in the user's
 # working directory, along with PBS output and error logs.
-#
-# Note that ILAMB will clobber the results of the previous run, which
-# are stored in the static $ILAMB_ROOT/OUTPUT directory.
 
 # Define model simulation type, CLM or CMIP5.
 export MODELTYPE=CMIP5
@@ -23,19 +20,30 @@ export SPATRES=0.5x0.5
 # Define plot file type: eps, pdf, png, or ps.
 export PLOTTYPE=png
 
-# Configure ILAMB deps NCL and ImageMagick.
+# Define directories.
 tools_dir=/home/csdms/tools
+nas_dir=/nas/data/ILAMB
+tmp_dir=/state/partition1
+
+# Define output name.
+job_id=`basename $PBS_JOBID .beach.colorado.edu`
+output_name=ILAMB-$PBS_O_LOGNAME-$job_id
+
+# Configure NCL and ImageMagick.
 export NCARG_ROOT=$tools_dir/ncl
 PATH=$NCARG_ROOT/bin:$tools_dir/ImageMagick/bin:$PATH
 
 # Configure ILAMB paths.
-export ILAMB_ROOT=/scratch/pbs/ilamb
+export ILAMB_ROOT=$tools_dir/ILAMB
 export ILAMB_CODESDIR=$ILAMB_ROOT/CODES
-export ILAMB_DATADIR=$ILAMB_ROOT/DATA
-export ILAMB_MODELSDIR=$ILAMB_ROOT/MODELS
-export ILAMB_OUTPUTDIR=$ILAMB_ROOT/OUTPUT
+export ILAMB_DATADIR=$nas_dir/DATA
+export ILAMB_MODELSDIR=$nas_dir/MODELS
+export ILAMB_OUTPUTDIR=$tmp_dir/$output_name
 stdout_file=$ILAMB_OUTPUTDIR/ILAMB.stdout
 stderr_file=$ILAMB_OUTPUTDIR/ILAMB.stderr
+
+# Copy OUTPUT to tmp directory for job.
+cp -R $nas_dir/OUTPUT $ILAMB_OUTPUTDIR
 
 # Run ILAMB.
 cd $ILAMB_CODESDIR
@@ -44,13 +52,13 @@ ncl -n main_ncl_code.ncl 1> $stdout_file 2> $stderr_file
 echo "ILAMB finish:" `date`
 
 # Package results.
-job_id=`basename $PBS_JOBID .beach.colorado.edu`
-tarfile=ILAMB-$PBS_O_LOGNAME-$job_id.tar.gz
-tar zcf $tarfile -C $ILAMB_ROOT OUTPUT
+tarfile=$output_name.tar.gz
+tar zcf $tarfile -C $tmp_dir $output_name
 mv $tarfile $PBS_O_WORKDIR
 
 # Cleanup.
 to_remove="$stdout_file $stderr_file \
+    $ILAMB_OUTPUTDIR \
     $ILAMB_CODESDIR/temp.data \
     $ILAMB_CODESDIR/tempfiles"
 for item in $to_remove; do
