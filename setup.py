f13e4b2942336a0cd1858a93bb9d83899bab5032
Mark Piper <mark.piper@colorado.edu>
2016-11-21 14:38:16 -0700

    Add setup and ez_setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+from pylamb import __version__
+
+
+setup(name='ILAMB',
+      version=__version__,
+      author='Mark Piper',
+      author_email='mark.piper@colorado.edu',
+      description='Python BMI for ILAMB',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+)
